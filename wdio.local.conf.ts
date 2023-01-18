e8ffd1be88a0726dd6b87135a6269501632f5ebc
davidjopcik <91221083+davidjopcik@users.noreply.github.com>
2023-01-18 15:16:19 +0100

    Update wdio.local.conf.ts

--- wdio.local.conf.ts
+++ wdio.local.conf.ts
@@ -8,9 +8,9 @@ export const config: WebdriverIO.Config = {
             browserName: 'chrome',
             acceptInsecureCerts: true,
 
-            /* 'wdio:devtoolsOptions': {
+             'wdio:devtoolsOptions': {
                 //headless: true
-            }, */
+            }, 
 
         }]
     }
